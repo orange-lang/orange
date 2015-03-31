ef67c1c1f9ce1c97b353a81ec475d1cface41ff8
Robert Fratto <robertfratto@gmail.com>
2015-03-31 18:00:01 -0500

    Added license notice to every file.

--- lib/gen/BinOpExpr.cc
+++ lib/gen/BinOpExpr.cc
@@ -1,3 +1,11 @@
+/*
+** Copyright 2014-2015 Robert Fratto. See the LICENSE.txt file at the top-level 
+** directory of this distribution.
+**
+** Licensed under the MIT license <http://opensource.org/licenses/MIT>. This file 
+** may not be copied, modified, or distributed except according to those terms.
+*/ 
+
 #include "gen/BinOpExpr.h"
 #include "gen/generator.h"
 #include "gen/VarExpr.h"
