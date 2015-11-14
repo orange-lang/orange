d5af8387947d003f3a8cf1843001e49c13744b50
Robert Fratto <robertfratto@gmail.com>
2015-11-14 17:29:43 -0500

    Add BinOpExpr, a base class to handle binary operator expressions.

---
+++ include/grove/BinOpExpr.h
@@ -0,0 +1,35 @@
+/*
+** Copyright 2014-2015 Robert Fratto. See the LICENSE.txt file at the top-level
+** directory of this distribution.
+**
+** Licensed under the MIT license <http://opensource.org/licenses/MIT>. This file
+** may not be copied, modified, or distributed except according to those terms.
+*/
+
+#pragma once
+
+#include "Expression.h"
+
+/**
+ * BinOpExpr is the base class for any grammar defined as expr operator expr.
+ */
+class BinOpExpr : public Expression {
+private:
+	Expression* m_LHS;
+	Expression* m_RHS;
+	
+	std::string m_operator;
+protected:
+	bool areTypesCompatible() const;
+	bool areTypesCastable() const;
+	bool requiresCast() const;
+public:
+	Expression* getLHS() const;
+	Expression* getRHS() const;
+	
+	std::string getOperator() const;
+	
+	virtual void resolve() override;
+	
+	BinOpExpr(Expression* LHS, std::string op, Expression* RHS);
+};
---
+++ lib/grove/BinOpExpr.cc
@@ -0,0 +1,74 @@
+/*
+** Copyright 2014-2015 Robert Fratto. See the LICENSE.txt file at the top-level
+** directory of this distribution.
+**
+** Licensed under the MIT license <http://opensource.org/licenses/MIT>. This file
+** may not be copied, modified, or distributed except according to those terms.
+*/
+
+#include <grove/BinOpExpr.h>
+#include <util/assertions.h>
+#include <grove/types/Type.h>
+
+bool BinOpExpr::areTypesCompatible() const
+{
+	if (getLHS()->getType() == getRHS()->getType())
+	{
+		return true;
+	}
+	
+	// If they're not the same type, determine if we can
+	// cast them.
+	return areTypesCastable();
+}
+
+bool BinOpExpr::areTypesCastable() const
+{
+	return getLHS()->getType()->isPODTy() && getRHS()->getType()->isPODTy();
+}
+
+bool BinOpExpr::requiresCast() const
+{
+	return getLHS()->getType() != getRHS()->getType();
+}
+
+Expression* BinOpExpr::getLHS() const
+{
+	return m_LHS;
+}
+
+Expression* BinOpExpr::getRHS() const
+{
+	return m_RHS;
+}
+
+std::string BinOpExpr::getOperator() const
+{
+	return m_operator;
+}
+
+void BinOpExpr::resolve()
+{
+	if (areTypesCompatible())
+	{
+		throw std::invalid_argument("LHS is not compatible with RHS");
+	}
+}
+
+BinOpExpr::BinOpExpr(Expression* LHS, std::string op, Expression* RHS)
+{
+	assertExists(LHS, "LHS must not be null.");
+	assertExists(RHS, "RHS must not be null.");
+	
+	if (op == "")
+	{
+		throw std::invalid_argument("op must not be empty");
+	}
+	
+	m_LHS = LHS;
+	m_operator = op;
+	m_RHS = RHS;
+	
+	addChild(LHS);
+	addChild(RHS);
+}
