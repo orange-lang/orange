c92ca3919a590eb24d3d48a7ddcae41f8385e4cd
Robert Fratto <robertfratto@gmail.com>
2015-11-15 17:32:45 -0500

    Add ExternFunction class for linking external functions.

---
+++ include/grove/ExternFunction.h
@@ -0,0 +1,37 @@
+/*
+** Copyright 2014-2015 Robert Fratto. See the LICENSE.txt file at the top-level
+** directory of this distribution.
+**
+** Licensed under the MIT license <http://opensource.org/licenses/MIT>. This file
+** may not be copied, modified, or distributed except according to those terms.
+*/
+
+#pragma once
+
+#include <vector>
+
+#include "Statement.h"
+#include "Typed.h"
+#include "Named.h"
+#include "Valued.h"
+
+class Parameter;
+class Type;
+
+class ExternFunction
+: public Statement, public Named, public Typed, public Valued
+{
+private:
+	std::vector<Parameter *> m_params;
+public:
+	virtual void build() override;
+	
+	/// Gets the list of parameters as a list of types.
+	std::vector<Type *> getParamTys() const;
+	
+	/// Gets the list of parameters.
+	std::vector<Parameter *> getParams() const;
+	
+	ExternFunction(std::string name, std::vector<Parameter *> params,
+				   Type* retType);
+};
---
+++ lib/grove/ExternFunction.cc
@@ -0,0 +1,65 @@
+/*
+** Copyright 2014-2015 Robert Fratto. See the LICENSE.txt file at the top-level
+** directory of this distribution.
+**
+** Licensed under the MIT license <http://opensource.org/licenses/MIT>. This file
+** may not be copied, modified, or distributed except according to those terms.
+*/
+
+#include <grove/types/FunctionType.h>
+
+#include <grove/ExternFunction.h>
+#include <grove/Parameter.h>
+#include <util/assertions.h>
+#include <llvm/IR/IRBuilder.h>
+#include <llvm/IR/Function.h>
+#include <grove/Module.h>
+
+
+std::vector<Type *> ExternFunction::getParamTys() const
+{
+	std::vector<Type *> tys;
+	
+	for (auto param : getParams())
+	{
+		tys.push_back(param->getType());
+	}
+	
+	return tys;
+}
+
+std::vector<Parameter *> ExternFunction::getParams() const
+{
+	return m_params;
+}
+
+void ExternFunction::build()
+{
+	auto func_ty = (llvm::FunctionType *)getType()->getLLVMType();
+	auto linkage = llvm::Function::ExternalLinkage;
+	
+	auto func = llvm::Function::Create(func_ty, linkage, getName(),
+									   getModule()->getLLVMModule());
+	
+	setValue(func);
+}
+
+ExternFunction::ExternFunction(std::string name, std::vector<Parameter *> params,
+							   Type* retType)
+{
+	if (m_name == "")
+	{
+		throw std::invalid_argument("name must not be empty.");
+	}
+	
+	for (auto param : params)
+	{
+		assertExists(param, "Param must not be null");
+		assertExists(param->getType(), "Param must have type.");
+	}
+	
+	assertExists(retType, "return type must exist");
+	
+	setType(FunctionType::get(retType, getParamTys()));
+	m_params = params;
+}
