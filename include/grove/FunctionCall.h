29d4070db2ee9d97a8d043e1bfbb23562e44bfa9
Robert Fratto <robertfratto@gmail.com>
2015-11-15 15:53:02 -0500

    Create FunctionCall that can call a function that takes zero arguments.

---
+++ include/grove/FunctionCall.h
@@ -0,0 +1,35 @@
+/*
+** Copyright 2014-2015 Robert Fratto. See the LICENSE.txt file at the top-level
+** directory of this distribution.
+**
+** Licensed under the MIT license <http://opensource.org/licenses/MIT>. This file
+** may not be copied, modified, or distributed except according to those terms.
+*/
+
+#pragma once 
+
+#include "Expression.h"
+
+namespace llvm { class Function; }
+
+class FunctionCall : public Expression {
+private:
+	std::string m_name;
+	std::vector<Expression *> m_args;
+	
+	llvm::Function* getFunction() const;
+public:
+	/// Gets the name of the function that we are calling.
+	std::string getName() const;
+	
+	/// Gets the arguments in this function call.
+	std::vector<Expression *> getArgs() const;
+	
+	virtual void resolve();
+	virtual void build();
+	
+	/// Constructs a new function call.
+	/// @param name The name of the function to call.
+	/// @param args The argument list of expressions. 
+	FunctionCall(std::string name, std::vector<Expression *> args);
+};
---
+++ lib/grove/FunctionCall.cc
@@ -0,0 +1,85 @@
+/*
+** Copyright 2014-2015 Robert Fratto. See the LICENSE.txt file at the top-level
+** directory of this distribution.
+**
+** Licensed under the MIT license <http://opensource.org/licenses/MIT>. This file
+** may not be copied, modified, or distributed except according to those terms.
+*/
+
+#include <grove/FunctionCall.h>
+#include <grove/Named.h>
+#include <grove/types/Type.h>
+#include <grove/types/FunctionType.h>
+#include <grove/types/FunctionType.h>
+#include <util/assertions.h>
+#include <llvm/IR/Value.h>
+#include <llvm/IR/IRBuilder.h>
+#include <llvm/IR/Function.h>
+
+std::string FunctionCall::getName() const
+{
+	return m_name;
+}
+
+std::vector<Expression *> FunctionCall::getArgs() const
+{
+	return m_args;
+}
+
+llvm::Function* FunctionCall::getFunction() const
+{
+	// Determine type
+	auto named = dynamic_cast<Valued *>(findNamed(getName()));
+	assertExists(named, "Couldn't find function");
+	
+	auto value = named->getValue();
+	
+	if (llvm::isa<llvm::Function>(value) == false)
+	{
+		throw std::invalid_argument("Object did not contain a function");
+	}
+	
+	return (llvm::Function *)value;
+}
+
+void FunctionCall::resolve()
+{
+	// Determine type
+	auto named = dynamic_cast<Typed *>(findNamed(getName()));
+	assertExists(named, "Couldn't find function");
+	
+	auto ty = named->getType();
+	if (ty == nullptr || ty->isFunctionTy() == false)
+	{
+    	assertExists(ty, "Object isn't a function.");
+	}
+	
+	auto func_ty = dynamic_cast<FunctionType*>(ty);
+	setType(func_ty->getReturnTy());
+}
+
+void FunctionCall::build()
+{
+	for (auto arg : m_args)
+	{
+		arg->build();
+	}
+	
+	IRBuilder()->CreateCall(getFunction());
+}
+
+FunctionCall::FunctionCall(std::string name, std::vector<Expression *> args)
+{
+	if (name == "")
+	{
+		throw std::invalid_argument("name cannot be empty");
+	}
+	
+	for (auto arg : args)
+	{
+		addChild(arg, true);
+	}
+	
+	m_name = name;
+	m_args = args;
+}
