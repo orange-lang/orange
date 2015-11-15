2ee17646797abf1cd908ca12be55a6388dc8922a
Robert Fratto <robertfratto@gmail.com>
2015-11-15 17:52:00 -0500

    Add StrValue to represent constant strings.

---
+++ lib/grove/StrValue.cc
@@ -0,0 +1,27 @@
+/*
+** Copyright 2014-2015 Robert Fratto. See the LICENSE.txt file at the top-level
+** directory of this distribution.
+**
+** Licensed under the MIT license <http://opensource.org/licenses/MIT>. This file
+** may not be copied, modified, or distributed except according to those terms.
+*/
+
+#include <grove/StrValue.h>
+
+#include <llvm/IR/IRBuilder.h>
+
+llvm::Value* StrValue::getValue() const
+{
+	return IRBuilder()->CreateConstGEP2_32(m_value, 0, 0);
+}
+
+void StrValue::build()
+{
+	auto val = IRBuilder()->CreateGlobalString(m_str);
+	setValue(val);
+}
+
+StrValue::StrValue(std::string value)
+{
+	m_str = value;
+}
