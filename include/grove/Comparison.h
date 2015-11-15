43461168ef84786116cd8cb25dc1714725b57971
Robert Fratto <robertfratto@gmail.com>
2015-11-14 20:42:28 -0500

    Add Comparison enum to give value to comparisons between two objects.

---
+++ include/grove/Comparison.h
@@ -0,0 +1,26 @@
+/*
+** Copyright 2014-2015 Robert Fratto. See the LICENSE.txt file at the top-level
+** directory of this distribution.
+**
+** Licensed under the MIT license <http://opensource.org/licenses/MIT>. This file
+** may not be copied, modified, or distributed except according to those terms.
+*/
+
+#pragma once 
+
+/// Defines some comparision between two items,
+/// where one item is the source and the other item is the target.
+enum Comparison
+{
+	/// Indicates that the two items are equal.
+	EQUAL,
+	
+	/// Indicates that the source is of lesser precedence than the target.
+	LOWER_PRECEDENCE,
+	
+	/// Indicates that the source is of greater precedence than the target.
+	HIGHER_PRECEDENCE,
+	
+	/// Indicates that the source and target cannot be compared.
+	INCOMPATIBLE
+};
