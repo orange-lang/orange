c218be2c1d4e9b3b37e78a115fcb19a94f3465f7
Robert Fratto <robertfratto@gmail.com>
2015-11-15 18:59:20 -0500

    Add NegativeExpr to get the negative value of signed or FP expressions.

---
+++ lib/grove/NegativeExpr.cc
@@ -0,0 +1,57 @@
+/*
+** Copyright 2014-2015 Robert Fratto. See the LICENSE.txt file at the top-level
+** directory of this distribution.
+**
+** Licensed under the MIT license <http://opensource.org/licenses/MIT>. This file
+** may not be copied, modified, or distributed except according to those terms.
+*/
+
+#include <grove/NegativeExpr.h>
+
+#include <grove/types/Type.h>
+
+#include <util/assertions.h>
+
+#include <llvm/IR/IRBuilder.h>
+
+Expression* NegativeExpr::getExpression() const
+{
+	return m_expr;
+}
+
+void NegativeExpr::resolve()
+{
+	auto ty = getExpression()->getType();
+	
+	if (ty->isPODTy() == false)
+	{
+		throw std::invalid_argument("can only negate a value that's numeric");
+	}
+	else if (ty->isIntTy() && ty->isSigned() == false)
+	{
+		throw std::invalid_argument("cannot negate an unsigned integer.");
+	}
+	else if (ty->isVoidTy())
+	{
+		throw std::invalid_argument("cannot negate a void value.");
+	}
+	
+	setType(ty);
+}
+
+void NegativeExpr::build()
+{
+	getExpression()->build();
+	assertExists(getExpression()->getValue(), "expression has no value.");
+	
+	auto val = IRBuilder()->CreateNeg(getExpression()->getValue());
+	setValue(val);
+}
+
+NegativeExpr::NegativeExpr(Expression* expr)
+{
+	assertExists(expr, "expr cannot be nullptr.");
+	m_expr = expr;
+	
+	addChild(expr);
+}
