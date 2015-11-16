4f84315fe02d826ecf0b8cf5611049e15e585b36
Robert Fratto <robertfratto@gmail.com>
2015-11-16 17:29:09 -0500

    Add comments for ASTNode::findNamed.

--- include/grove/ASTNode.h
+++ include/grove/ASTNode.h
@@ -6,16 +6,16 @@
 ** may not be copied, modified, or distributed except according to those terms.
 */
 
-#pragma once 
+#pragma once
 
 #include <vector>
 
 namespace llvm {
 	class ConstantFolder;
-	
+
 	template <bool preserveNames>
 	class IRBuilderDefaultInserter;
-	
+
 	template <bool preserveNames, typename T, typename Inserter>
 	class IRBuilder;
 }
@@ -34,69 +34,69 @@ class ASTNode {
 private:
 	Module* m_module = nullptr;
 	ASTNode* m_parent = nullptr;
-	
+
 	std::vector<ASTNode *> m_children;
 public:
 	/// Gets the module this node resides in.
 	Module* getModule() const;
-	
+
 	/// Gets the parent of this node.
 	ASTNode* getParent() const;
-	
+
 	std::vector<ASTNode *> getChildren() const;
-	
+
 	/// Gets the IR builder in the module.
 	IRBuilder* IRBuilder() const;
-	
+
 	void addChild(ASTNode* child, bool mustExist = false);
-	
+
 	bool isRootNode() const;
-	
+
 	/// Creates a copy of this node.
 	virtual ASTNode* copy() const;
-	
+
 	virtual void resolve();
-	
+
 	/// Generate code. Sets value if applicable.
 	virtual void build();
-	
+
 	/// Returns whether or not this node is a type.
 	template <typename T> bool is()
 	{
 		return dynamic_cast<T>(this) != nullptr;
 	}
-	
+
 	/// Casts this node to a certain type.
-	template <typename T> T as() 
+	template <typename T> T as()
 	{
 		auto casted = dynamic_cast<T>(this);
-		
+
 		if (casted == nullptr)
 		{
 			throw std::runtime_error("object could not be casted to type");
 		}
-		
+
 		return casted;
 	}
-	
+
 	/// Finds a parent of a type T.
 	template <typename T> T findParent() const
 	{
 		ASTNode* ptr = getParent();
-		
+
 		while (ptr != nullptr)
 		{
 			if (ptr->is<T>())
 			{
 				return ptr->as<T>();
 			}
-			
+
 			ptr = ptr->getParent();
 		}
-		
+
 		return nullptr;
 	}
-	
+
 	/// Finds the first child, depth-first, of a type T.
 	template <typename T> T findChild() const
 	{
@@ -106,49 +106,53 @@ public:
 			{
 				return child->as<T>();
 			}
-			
+
 			auto val = child->findChild<T>();
 			if (val != nullptr)
 			{
 				return val;
 			}
 		}
-				
+
 		return nullptr;
 	}
-	
+
 	/// Finds all children, depth-first, of a type T.
 	template <typename T> std::vector<T> findChildren() const
 	{
 		std::vector<T> children;
-		
+
 		for (auto child : getChildren())
 		{
 			if (child->is<T>())
 			{
 				children.push_back(child->as<T>());
 			}
-			
+
 			auto vals = child->findChildren<T>();
 			children.insert(children.end(), vals.begin(), vals.end());
 		}
-		
+
 		return children;
 	}
-	
+
+	/// Finds a named node in the AST. Searches up to this node.
 	Named* findNamed(std::string name) const;
-	
+
+	/// Finds a named node in the AST with a list of candidates to help
+	/// resolve nodes that may have duplicates (functions). Searches
+	/// up to this node.
 	Named* findNamed(std::string name, std::vector<Type *> candidates) const;
 
 	/// Constructs a new root node with a module.
 	ASTNode(Module* module);
-	
-	/// Constructs a child node with a parent. 
+
+	/// Constructs a child node with a parent.
 	ASTNode(ASTNode* parent);
-	
+
 	/// Constructs an orphaned node.
 	/// It is up to the parent to add it as a child.
 	ASTNode();
-	
+
 	virtual ~ASTNode();
 };
