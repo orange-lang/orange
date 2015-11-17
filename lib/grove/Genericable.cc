2c479370dcace3e338241c2698a8bd0b3248fe1c
Robert Fratto <robertfratto@gmail.com>
2015-11-17 17:11:16 -0500

    Add Genericable interface.
    Represents a node that *may* be a generic (a node is generic if any of its types are var).

---
+++ lib/grove/Genericable.cc
@@ -0,0 +1,54 @@
+/*
+** Copyright 2014-2015 Robert Fratto. See the LICENSE.txt file at the top-level
+** directory of this distribution.
+**
+** Licensed under the MIT license <http://opensource.org/licenses/MIT>. This file
+** may not be copied, modified, or distributed except according to those terms.
+*/
+
+#include <grove/Genericable.h>
+#include <grove/Typed.h>
+#include <grove/types/Type.h>
+
+#include <util/assertions.h>
+
+bool Genericable::isGeneric() const
+{
+	return false;
+}
+
+Genericable* Genericable::createInstance(Type *type)
+{
+	if (isGeneric() == false)
+	{
+		throw std::runtime_error("cannot create instance of non-generic");
+	}
+	
+	return nullptr;
+}
+
+Genericable* Genericable::findInstance(Type *type) const
+{
+	if (isGeneric() == false)
+	{
+		throw std::runtime_error("cannot find instance of non-generic");
+	}
+	
+	for (auto instance : m_instances)
+	{
+		auto ty = instance->getType();
+		assertExists(ty, "Instance did not have a type!");
+		
+		if (ty == type)
+		{
+			return instance;
+		}
+	}
+	
+	return nullptr;
+}
+
+bool Genericable::hasInstance(Type *type) const
+{
+	return findInstance(type) != nullptr;
+}
