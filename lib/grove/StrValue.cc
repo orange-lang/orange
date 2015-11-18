689915e6a975862d0e0aec2a86a35bfba850ccc2
Robert Fratto <robertfratto@gmail.com>
2015-11-17 21:31:08 -0500

    Replace literal special characters in StrValue.

--- lib/grove/StrValue.cc
+++ lib/grove/StrValue.cc
@@ -13,6 +13,25 @@
 
 #include <llvm/IR/IRBuilder.h>
 
+const void replaceAll(std::string& str, const std::string& from,
+					  const std::string& to)
+{
+	if (from.empty())
+	{
+		return;
+	}
+	
+	std::string::size_type start_pos = 0;
+	
+	while ((start_pos = str.find(from, start_pos)) != std::string::npos)
+	{
+		str.replace(start_pos, from.length(), to);
+		
+		// Add length in case 'to' contains 'from', like replacing 'x' with 'yx'
+		start_pos += to.length(); 
+	}
+}
+
 llvm::Value* StrValue::getValue() const
 {
 	return IRBuilder()->CreateConstGEP2_32(m_value, 0, 0);
@@ -27,5 +46,9 @@ void StrValue::build()
 StrValue::StrValue(std::string value)
 {
 	m_str = value.substr(1, value.size() - 2);
+	replaceAll(m_str, "\\n", "\n");
+	replaceAll(m_str, "\\t", "\t");
+	replaceAll(m_str, "\\r", "\r");
+
 	setType(PointerType::get(IntType::get(8)));
 }
