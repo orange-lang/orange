13db6e60418341606b835ba975c30599ee530a62
Robert Fratto <robertfratto@gmail.com>
2015-11-18 20:19:36 -0500

    Add function to copy vector of an ObjectBase type.

---
+++ include/util/copy.h
@@ -0,0 +1,22 @@
+/*
+** Copyright 2014-2015 Robert Fratto. See the LICENSE.txt file at the top-level
+** directory of this distribution.
+**
+** Licensed under the MIT license <http://opensource.org/licenses/MIT>. This file
+** may not be copied, modified, or distributed except according to those terms.
+*/
+
+#pragma once 
+
+template <typename T>
+std::vector<T> copyVector(std::vector<T> orig)
+{
+	std::vector<T> ret;
+	
+	for (auto element : orig)
+	{
+		ret.push_back(element->copy()->template as<T>());
+	}
+	
+	return ret;
+}
