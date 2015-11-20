73872789c16db692a527a13d50a22667a50d12df
Robert Fratto <robertfratto@gmail.com>
2015-11-20 17:21:42 -0500

    Add IncrementExpr.
    Handles pre- and post- ++ and --.

---
+++ include/grove/IncrementExpr.h
@@ -0,0 +1,33 @@
+/*
+** Copyright 2014-2015 Robert Fratto. See the LICENSE.txt file at the top-level
+** directory of this distribution.
+**
+** Licensed under the MIT license <http://opensource.org/licenses/MIT>. This file
+** may not be copied, modified, or distributed except according to those terms.
+*/
+
+#pragma once 
+
+#include "Expression.h"
+
+class IncrementExpr : public Expression
+{
+private:
+	Expression* m_expr = nullptr;
+	bool m_preincrement = false;
+	int m_delta = 0;
+public:
+	Expression* getExpression() const;
+	
+	virtual ASTNode* copy() const override;
+	
+	virtual void resolve() override;
+	virtual void build() override;
+	
+	int getDelta() const;
+	
+	bool isPreIncrement() const;
+	bool isPostIncrement() const;
+	
+	IncrementExpr(Expression* expr, int delta, bool preincrement);
+};
