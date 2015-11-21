7e040adb25a67fdcbb497a8259d91cd80afcc35e
Robert Fratto <robertfratto@gmail.com>
2015-11-21 18:03:03 -0500

    Add DerefExpr.
    Supports dereferencing pointers.

---
+++ lib/grove/DerefExpr.cc
@@ -0,0 +1,65 @@
+/*
+** Copyright 2014-2015 Robert Fratto. See the LICENSE.txt file at the top-level
+** directory of this distribution.
+**
+** Licensed under the MIT license <http://opensource.org/licenses/MIT>. This file
+** may not be copied, modified, or distributed except according to those terms.
+*/
+
+#include <grove/DerefExpr.h>
+
+#include <grove/types/Type.h>
+
+#include <util/assertions.h>
+
+#include <llvm/IR/IRBuilder.h>
+
+ASTNode* DerefExpr::copy() const
+{
+	auto expr = getExpression()->copy()->as<Expression *>();
+	return new DerefExpr(expr);
+}
+
+Expression* DerefExpr::getExpression() const
+{
+	return m_expression;
+}
+
+bool DerefExpr::hasPointer() const
+{
+	return true;
+}
+
+llvm::Value* DerefExpr::getPointer() const
+{
+	return m_expression->getValue();
+}
+
+void DerefExpr::resolve()
+{
+	auto ty = getExpression()->getType();
+	assertExists(ty, "Expression has no type");
+	
+	if (ty->isPointerTy() == false)
+	{
+		throw std::runtime_error("Cannot derefence type that's not a pointer");
+	}
+	
+	setType(ty->getBaseTy());
+}
+
+void DerefExpr::build()
+{
+	getExpression()->build();
+	
+	auto val = getExpression()->getValue();
+	assertExists(val, "Expression has no value.");
+	
+	setValue(IRBuilder()->CreateLoad(val));
+}
+
+DerefExpr::DerefExpr(Expression* expr)
+{
+	m_expression = expr;
+	addChild(m_expression, true);
+}
