d4fe6fa8150aad7f727939555c12a3fc9794666a
Robert Fratto <robertfratto@gmail.com>
2015-11-21 21:24:21 -0500

    Add ArrayAccessExpr to get values in arrays.

---
+++ lib/grove/ArrayAccessExpr.cc
@@ -0,0 +1,110 @@
+/*
+** Copyright 2014-2015 Robert Fratto. See the LICENSE.txt file at the top-level
+** directory of this distribution.
+**
+** Licensed under the MIT license <http://opensource.org/licenses/MIT>. This file
+** may not be copied, modified, or distributed except according to those terms.
+*/
+
+#include <grove/ArrayAccessExpr.h>
+#include <grove/Module.h>
+
+#include <grove/types/Type.h>
+
+#include <util/assertions.h>
+
+#include <llvm/IR/Constants.h>
+#include <llvm/IR/IRBuilder.h>
+
+ASTNode* ArrayAccessExpr::copy() const
+{
+	auto array_copy = getArray()->copy()->as<Expression *>();
+	auto idx_copy = getIndex()->copy()->as<Expression *>();
+	return new ArrayAccessExpr(array_copy, idx_copy);
+}
+
+Expression* ArrayAccessExpr::getArray() const
+{
+	return m_array;
+}
+
+Expression* ArrayAccessExpr::getIndex() const
+{
+	return m_idx;
+}
+
+void ArrayAccessExpr::resolve()
+{
+	auto array_ty = getArray()->getType();
+	assertExists(array_ty, "Array has no type");
+	
+	bool valid = array_ty->isArrayTy() || array_ty->isPointerTy();
+	if (valid == false)
+	{
+		throw std::runtime_error("Can only use [] with arrays or pointers");
+	}
+	
+	auto idx_ty = getIndex()->getType();
+	assertExists(idx_ty, "Index has no type");
+	
+	if (idx_ty->isIntTy() == false)
+	{
+		throw std::runtime_error("Can only use ints to access arrays");
+	}
+	
+	setType(array_ty->getBaseTy());
+}
+
+bool ArrayAccessExpr::hasPointer() const
+{
+	return true;
+}
+
+llvm::Value* ArrayAccessExpr::getPointer() const
+{
+	return m_value;
+}
+
+llvm::Value* ArrayAccessExpr::getValue() const
+{
+	return IRBuilder()->CreateLoad(m_value);
+}
+
+void ArrayAccessExpr::build()
+{
+	getArray()->build();
+	getIndex()->build();
+	
+	llvm::Value* vArray = getArray()->getValue();
+	if (getArray()->hasPointer() && getArray()->getType()->isArrayTy())
+	{
+		vArray = getArray()->getPointer();
+	}
+	
+	auto vIndex = getIndex()->getValue();
+	
+	assertExists(vArray, "Array generated no value");
+	assertExists(vIndex, "Index generated no value");
+	
+	std::vector<llvm::Value *> indices;
+	
+	if (getArray()->getType()->isArrayTy())
+	{
+		auto& ctx = getModule()->getLLVMContext();
+		auto idx = llvm::ConstantInt::get(llvm::Type::getInt64Ty(ctx), 0);
+		indices.push_back(idx);
+	}
+	
+	indices.push_back(vIndex);
+	
+	setValue(IRBuilder()->CreateInBoundsGEP(vArray, indices));
+}
+
+ArrayAccessExpr::ArrayAccessExpr(Expression* array, Expression* idx)
+{
+	m_array = array;
+	m_idx = idx;
+	
+	addChild(m_array, true);
+	addChild(m_idx, true);
+}
