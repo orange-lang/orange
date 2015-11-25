4f24f8683391375d297896a90f1f290d9145c8fd
Robert Fratto <robertfratto@gmail.com>
2015-11-25 00:22:06 -0500

    Add SizeofExpr.
    Enables getting the size of an expression or type in bytes.

---
+++ include/grove/SizeofExpr.h
@@ -0,0 +1,35 @@
+/*
+ ** Copyright 2014-2015 Robert Fratto. See the LICENSE.txt file at the top-level
+ ** directory of this distribution.
+ **
+ ** Licensed under the MIT license <http://opensource.org/licenses/MIT>. This file
+ ** may not be copied, modified, or distributed except according to those terms.
+ */
+
+#pragma once
+
+#include "Expression.h"
+
+/**
+ * Gets the size of an expression or a type
+ */
+class SizeofExpr : public Expression
+{
+private:
+	Expression* m_expression_arg = nullptr;
+	Type* m_type_arg = nullptr;
+public:
+	virtual ASTNode* copy() const override;
+	
+	/// Gets the expression, if any, to get the size of.
+	Expression* getExpressionArg() const;
+	
+	/// Gets the type, if any, to get the size of.
+	Type* getTypeArg() const;
+	
+	virtual void resolve() override;
+	virtual void build() override;
+	
+	SizeofExpr(Type* typeArg);
+	SizeofExpr(Expression* expr);
+};
