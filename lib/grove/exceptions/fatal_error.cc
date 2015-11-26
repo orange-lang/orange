54f97a4c53c8dbb722318ffa00d88d6cee954883
Robert Fratto <robertfratto@gmail.com>
2015-11-26 15:52:07 -0500

    Add fatal_error for fatal compiler bugs.

--- lib/grove/exceptions/fatal_error.cc
+++ lib/grove/exceptions/fatal_error.cc
@@ -5,3 +5,22 @@
 ** Licensed under the MIT license <http://opensource.org/licenses/MIT>. This file
 ** may not be copied, modified, or distributed except according to those terms.
 */
+
+#include <grove/exceptions/fatal_error.h>
+#include <sstream>
+
+const char* fatal_error::what() const noexcept
+{
+	return m_error.c_str();
+}
+
+fatal_error::fatal_error(std::string msg)
+{
+	std::stringstream ss;
+	ss << "FATAL ERROR: " << msg << "\n\n";
+	ss << "This is probably a compiler bug! Please report this issue with ";
+	ss << "the error you're experiencing, how you invoked orange ";
+	ss << "and the code that caused this issue.";
+	
+	m_error = ss.str();
+}
