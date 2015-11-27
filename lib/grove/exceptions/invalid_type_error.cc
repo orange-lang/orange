f93ff7019367ed7e87bf15a779d78a1a342d8c1c
Robert Fratto <robertfratto@gmail.com>
2015-11-27 13:02:39 -0500

    Add invalid_type_error
    Used to represent an operation where an invalid type is involved.

---
+++ lib/grove/exceptions/invalid_type_error.cc
@@ -0,0 +1,30 @@
+/*
+** Copyright 2014-2015 Robert Fratto. See the LICENSE.txt file at the top-level
+** directory of this distribution.
+**
+** Licensed under the MIT license <http://opensource.org/licenses/MIT>. This file
+** may not be copied, modified, or distributed except according to those terms.
+*/
+
+#include <grove/exceptions/invalid_type_error.h>
+#include <grove/exceptions/fatal_error.h>
+
+#include <grove/types/Type.h>
+
+invalid_type_error::invalid_type_error(CodeBase* element, std::string desc,
+									   Type* ty)
+: code_error(element)
+{
+	if (ty == nullptr)
+	{
+		throw fatal_error("ty is nullptr");
+	}
+	
+	std::stringstream ss;
+	
+	ss << fileWithPosition(element) << ": error: "
+	   << desc << " " << ty->getString() << "\n"
+	   << getContext(element);
+	
+	m_error = ss.str();
+}
