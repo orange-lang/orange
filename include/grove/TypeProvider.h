dccadc40f3b1c18c2400ed0a7cc051a655164834
Robert Fratto <robertfratto@gmail.com>
2015-11-28 16:25:18 -0500

    Add TypeProvider
    specifies a node that can provide a type to a NodeType instance.

---
+++ include/grove/TypeProvider.h
@@ -0,0 +1,20 @@
+/*
+** Copyright 2014-2015 Robert Fratto. See the LICENSE.txt file at the top-level
+** directory of this distribution.
+**
+** Licensed under the MIT license <http://opensource.org/licenses/MIT>. This file
+** may not be copied, modified, or distributed except according to those terms.
+*/
+
+#pragma once 
+
+#include "CodeBase.h"
+
+/**
+ * TypeProvider specifies nodes that can provide a type to a 
+ * NodeType.
+ */
+class TypeProvider : public CodeBase
+{
+	
+};
