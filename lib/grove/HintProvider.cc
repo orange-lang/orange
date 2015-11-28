30918aecfd14b14ac02288ecfc5d66ac98e83806
Robert Fratto <robertfratto@gmail.com>
2015-11-28 14:32:02 -0500

    Implement HintProvider.

---
+++ lib/grove/HintProvider.cc
@@ -0,0 +1,14 @@
+/*
+** Copyright 2014-2015 Robert Fratto. See the LICENSE.txt file at the top-level
+** directory of this distribution.
+**
+** Licensed under the MIT license <http://opensource.org/licenses/MIT>. This file
+** may not be copied, modified, or distributed except according to those terms.
+*/
+
+#include <grove/HintProvider.h>
+
+CodeBase* HintProvider::getHint() const
+{
+	throw fatal_error("HintProvider::getHint not overriden");
+}
