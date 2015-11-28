b0a396b76b786559234bbe93f604347d6c85e60a
Robert Fratto <robertfratto@gmail.com>
2015-11-28 16:33:32 -0500

    Add ReferenceType.
    Represents a NodeType that will find a TypeProvider and use it for a type.

---
+++ include/grove/types/ReferenceType.h
@@ -0,0 +1,23 @@
+/*
+** Copyright 2014-2015 Robert Fratto. See the LICENSE.txt file at the top-level
+** directory of this distribution.
+**
+** Licensed under the MIT license <http://opensource.org/licenses/MIT>. This file
+** may not be copied, modified, or distributed except according to those terms.
+*/
+
+#pragma once 
+
+#include "NodeType.h"
+
+class ReferenceType : public NodeType
+{
+protected:
+	OString m_name;
+public:
+	OString getName() const;
+	
+	virtual void resolve();
+	
+	ReferenceType(OString name);
+};
---
+++ lib/grove/types/ReferenceType.cc
@@ -0,0 +1,57 @@
+/*
+** Copyright 2014-2015 Robert Fratto. See the LICENSE.txt file at the top-level
+** directory of this distribution.
+**
+** Licensed under the MIT license <http://opensource.org/licenses/MIT>. This file
+** may not be copied, modified, or distributed except according to those terms.
+*/
+
+#include <grove/types/ReferenceType.h>
+
+#include <grove/Typed.h>
+#include <grove/Named.h>
+#include <grove/TypeProvider.h>
+
+#include <grove/exceptions/code_error.h>
+
+#include <util/assertions.h>
+
+OString ReferenceType::getName() const
+{
+	return m_name;
+}
+
+void ReferenceType::resolve()
+{
+	SearchSettings settings;
+	settings.filter = [] (Named *named)
+	{
+		return named->is<TypeProvider *>();
+	};
+	
+	auto named = findNamed(m_name, nullptr, settings);
+	if (named == nullptr)
+	{
+		auto name = m_name;
+		throw code_error(this, [name] () -> std::string
+			{
+				std::stringstream ss;
+				ss << "no type named " << name.str() << " found";
+				return ss.str();
+			});
+	}
+	
+	assertExists(named->as<Typed *>()->getType(), "node has no type");
+	m_type = named->as<Typed *>()->getType()->getLLVMType();
+}
+
+ReferenceType::ReferenceType(OString name)
+: NodeType(false)
+{
+	if (name == "")
+	{
+		throw fatal_error("name for ReferenceType was empty");
+	}
+	
+	m_name = name;
+}
