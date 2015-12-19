1e38ddfead356b1f17625c1f99bd99f38832ce97
Robert Fratto <robertfratto@gmail.com>
2015-12-19 17:36:41 -0500

    Add NodeReference.
    Similar to IDReference, but references an explicit node.

---
+++ include/grove/NodeReference.h
@@ -0,0 +1,54 @@
+/*
+** Copyright 2014-2015 Robert Fratto. See the LICENSE.txt file at the top-level
+** directory of this distribution.
+**
+** Licensed under the MIT license <http://opensource.org/licenses/MIT>. This file
+** may not be copied, modified, or distributed except according to those terms.
+*/
+
+#pragma once
+
+#include "Expression.h"
+#include "Accessible.h"
+
+class Valued;
+
+/**
+ * NodeReference is a class that can refer to any ID by name.
+ */
+class NodeReference : public Expression, public Accessible {
+private:
+	ASTNode* m_node = nullptr;
+protected:
+	void setNode(ASTNode* node);
+	
+	NodeReference();
+public:
+	const Valued* nodeAsValued() const;
+	
+	const ASTNode* getNode() const;
+	
+	virtual llvm::Value* getPointer() const override;
+	
+	virtual bool hasPointer() const override;
+	
+	virtual bool transferrable() const override;
+	
+	virtual llvm::Value* getValue() const override;
+	
+	virtual llvm::Value* getSize() const override;
+	
+	virtual bool isAccessible() const override;
+	
+	virtual Expression* access(OString name, const ASTNode* hint) const override;
+	
+	virtual void findDependencies() override;
+	
+	virtual void resolve() override;
+	
+	virtual void build() override;
+	
+	virtual ASTNode* copy() const override;
+	
+	NodeReference(ASTNode* node);
+};
