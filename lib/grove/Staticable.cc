7e2bb8fec593580940acd1175027b5ff07eb2f2a
Robert Fratto <robertfratto@gmail.com>
2015-12-30 13:17:34 -0500

    Add Staticable

---
+++ lib/grove/Staticable.cc
@@ -0,0 +1,19 @@
+/*
+** Copyright 2014-2015 Robert Fratto. See the LICENSE.txt file at the top-level
+** directory of this distribution.
+**
+** Licensed under the MIT license <http://opensource.org/licenses/MIT>. This file
+** may not be copied, modified, or distributed except according to those terms.
+*/
+
+#include <grove/Staticable.h>
+
+void Staticable::setStatic(bool isStatic)
+{
+	m_static = isStatic;
+}
+
+const bool Staticable::getStatic() const
+{
+	return m_static;
+}
