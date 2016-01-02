20f78acda01c584b8034c3917233d67f62b4c9f8
Robert Fratto <robertfratto@gmail.com>
2016-01-02 14:44:56 -0500

    Allow for opaque class types
    Opaque class types allow for forward referencing the body of a class.

--- include/grove/types/ClassType.h
+++ include/grove/types/ClassType.h
@@ -20,18 +20,27 @@ class Orange::ClassType : public Orange::Type
 private:
 	std::vector<const Type *> m_members;
 	ClassDecl* m_class = nullptr;
+	
+	bool m_opaque = false;
 protected:
+	// Creates a ClassType with members
 	ClassType(ClassDecl* the_class, std::vector<const Type *> members);
+	
+	// Creates an opaque ClassType where the members can be specified
+	// later
+	ClassType(ClassDecl* the_class);
 public:
 	static std::string getSignature(const ClassDecl* name,
-									const std::vector<const Type *> members);
+									const std::vector<const Type *> members,
+									bool opaque);
 	
 	virtual bool isClassTy() const override;
 	virtual bool isAggTy() const override;
 	
 	virtual bool hasReference() const override;
-	
 	virtual ASTNode* getReference() const override;
+	
+	bool isOpaque() const;
 	
 	ClassDecl* getClass() const;
 	
@@ -44,6 +53,12 @@ public:
 	virtual std::string getSignature() const override;
 	
 	virtual const Type* copyType() const override;
+	
+	/// Specifies the members of an opaque class.
+	/// Fails if this type isn't opaque.
+	void specifyMembers(std::vector<const Type *> members);
+	
+	static ClassType* get(Module* mod, ClassDecl* the_class);
 	
 	static ClassType* get(Module* mod, ClassDecl* the_class,
 						  std::vector<const Type *> members);
--- lib/grove/types/ClassType.cc
+++ lib/grove/types/ClassType.cc
@@ -12,7 +12,18 @@
 
 #include <llvm/IR/DerivedTypes.h>
 
-Orange::ClassType::ClassType(ClassDecl* the_class, std::vector<const Type *> members)
+Orange::ClassType::ClassType(ClassDecl* the_class)
+: Type(false), m_class(the_class)
+{
+	std::stringstream name;
+	name << "class." << the_class->getName().str();
+	
+	m_type = llvm::StructType::create(*m_context, name.str());
+	m_opaque = true;
+}
+
+Orange::ClassType::ClassType(ClassDecl* the_class,
+							 std::vector<const Type *> members)
 : Type(false), m_class(the_class)
 {
 	m_members = members;
@@ -42,9 +53,15 @@ const Orange::Type* Orange::ClassType::copyType() const
 }
 
 std::string Orange::ClassType::getSignature(const ClassDecl* the_class,
-									const std::vector<const Type *> members)
+									const std::vector<const Type *> members,
+									bool opaque)
 {
 	std::stringstream ss;
+	
+	if (opaque)
+	{
+		ss << "O" << the_class;
+	}
 	ss << "T" << members.size();
 	for (auto member : members)
 	{
@@ -93,6 +110,25 @@ const
 	return ClassType::get(m_module, m_class, new_members);
 }
 
+void Orange::ClassType::specifyMembers(std::vector<const Type *> members)
+{
+	if (m_opaque == false)
+	{
+		throw fatal_error("Type is not opaque");
+	}
+	
+	m_members = members;
+	
+	std::vector<llvm::Type*> elements;
+	for (auto member : m_members)
+	{
+		elements.push_back(member->getLLVMType());
+	}
+	
+	auto struct_ty = (llvm::StructType *)m_type;
+	struct_ty->setBody(elements);
+}
+
 std::vector<const Orange::Type*> Orange::ClassType::getMemberTys() const
 {
 	return m_members;
@@ -119,7 +155,24 @@ std::string Orange::ClassType::getString() const
 
 std::string Orange::ClassType::getSignature() const
 {
-	return getSignature(m_class, m_members);
+	return getSignature(m_class, m_members, m_opaque);
+}
+
+Orange::ClassType* Orange::ClassType::get(Module* mod, ClassDecl* the_class)
+{
+	std::vector<const Type*> members;
+	
+	auto sig = getSignature(the_class, members, true);
+	auto defined = getDefined(mod, sig);
+	if (defined != nullptr)
+	{
+		return defined->as<ClassType *>();
+	}
+	
+	ClassType* ty = new ClassType(the_class);
+	define(mod, sig, ty);
+	
+	return ty;
 }
 
 Orange::ClassType* Orange::ClassType::get(Module* mod, ClassDecl* the_class,
@@ -141,7 +194,7 @@ Orange::ClassType* Orange::ClassType::get(Module* mod, ClassDecl* the_class,
 		}
 	}
 	
-	auto sig = getSignature(the_class, members);
+	auto sig = getSignature(the_class, members, false);
 	auto defined = getDefined(mod, sig);
 	if (defined != nullptr)
 	{
