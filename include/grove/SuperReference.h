36037fd2de156afb6cd01453d93f9e4d95f763ac
Robert Fratto <robertfratto@gmail.com>
2016-01-02 17:59:30 -0500

    Add SuperReference
    Represents the reference to the super class.

---
+++ include/grove/SuperReference.h
@@ -0,0 +1,16 @@
+/*
+** Copyright 2014-2016 Robert Fratto. See the LICENSE.txt file at the top-level
+** directory of this distribution.
+**
+** Licensed under the MIT license <http://opensource.org/licenses/MIT>. This file
+** may not be copied, modified, or distributed except according to those terms.
+*/
+
+#pragma once
+
+#include "IDReference.h"
+
+class SuperReference : public IDReference {
+public:
+	SuperReference();
+};
