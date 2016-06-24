44dd599d700b5846affd8862b435e48e9f0c1ca2
Robert Fratto <robertfratto@gmail.com>
2016-06-23 22:59:54 -0400

    Add default and custom ctors for all AST nodes

--- include/libast/ast.h
+++ include/libast/ast.h
@@ -57,6 +57,10 @@ namespace orange { namespace ast {
 	};
 
 	struct Node {
+	protected:
+		Node() { }
+
+	public:
 		int id;
 		std::vector<Flag*> flags;
 
@@ -77,45 +81,85 @@ namespace orange { namespace ast {
 	// Statements
 	//
 
-	struct Statement : Node { };
+	struct Statement : Node {
+	protected:
+		Statement() { }
+	};
 
 	struct PackageStmt : Statement {
 		std::string name;
+
+		PackageStmt() { }
+
+		PackageStmt(std::string name) : name(name) { }
 	};
 
 	struct UsingStmt : Statement {
 		std::string name;
 		std::string alias;
+
+		UsingStmt() { }
+
+		UsingStmt(std::string name, std::string alias) :
+			name(name), alias(alias) { }
 	};
 
 	struct ClassStmt : Statement {
 		std::string name;
 		std::vector<NamedType*> implements;
 		BlockExpr* body;
+
+		ClassStmt() { }
+
+		ClassStmt(std::string name, std::vector<NamedType*> implements, BlockExpr* body) :
+			name(name), implements(implements), body(body) { }
 	};
 
 	struct EnumMember : Statement {
 		std::string name;
 		std::vector<ParamStmt*> params;
+
+		EnumMember() { }
+
+		EnumMember(std::string name, std::vector<ParamStmt*> params) :
+			name(name), params(params) { }
 	};
 
 	struct EnumStmt : Statement {
 		std::string name;
 		std::vector<EnumMember*> members;
+
+		EnumStmt() { }
+
+		EnumStmt(std::string name, std::vector<EnumMember*> members) :
+			name(name), members(members) { }
 	};
 
 	struct VarDeclStmt : Statement {
 		Type* type;
 		std::vector<std::string> bindings;
 		Expression* value;
+
+		VarDeclStmt() { }
+
+		VarDeclStmt(Type* type, std::vector<std::string> bindings, Expression* value) :
+			type(type), bindings(bindings), value(value) { }
 	};
 
 	struct GetterStmt : Statement {
 		BlockExpr* body;
+
+		GetterStmt() { }
+
+		GetterStmt(BlockExpr* body) : body(body) { }
 	};
 
 	struct SetterStmt : Statement {
 		BlockExpr* body;
+
+		SetterStmt() { }
+
+		SetterStmt(BlockExpr* body) : body(body) { }
 	};
 
 	struct PropertyStmt : Statement {
@@ -123,6 +167,11 @@ namespace orange { namespace ast {
 		std::string name;
 		GetterStmt* getter;
 		SetterStmt* setter;
+
+		PropertyStmt() { }
+
+		PropertyStmt(Type* type, std::string name, GetterStmt* getter, SetterStmt* setter) :
+			type(type), name(name), getter(getter), setter(setter) { }
 	};
 
 	struct FuncDeclStmt : Statement {
@@ -131,79 +180,149 @@ namespace orange { namespace ast {
 		std::vector<Type*> generics;
 		std::vector<TypeConstraint*> constraints;
 		std::vector<ParamStmt*> params;
+
+		FuncDeclStmt() { }
+
+		FuncDeclStmt(Type* retType, std::string name, std::vector<Type*> generics,
+		             std::vector<TypeConstraint*> constraints, std::vector<ParamStmt*> params) :
+			retType(retType), name(name), generics(generics), constraints(constraints),
+			params(params) { }
 	};
 
-	struct ExternFuncDeclStmt : FuncDeclStmt { };
+	struct ExternFuncDeclStmt : FuncDeclStmt {
+		using FuncDeclStmt::FuncDeclStmt;
+	};
 
 	struct InterfaceStmt : Statement {
 		std::string name;
 		std::vector<FuncDeclStmt*> functions;
+
+		InterfaceStmt() { }
+
+		InterfaceStmt(std::string name, std::vector<FuncDeclStmt*> functions) :
+			name(name), functions(functions) { }
 	};
 
 	struct ParamStmt : Statement {
 		std::string name;
 		Type* type;
 		Expression* value;
+
+		ParamStmt() { }
+
+		ParamStmt(std::string name, Type* type, Expression* value) :
+			name(name), type(type), value(value) { }
 	};
 
 	struct ExtendsStmt : Statement {
 		NamedType* base;
 		BlockExpr* body;
+
+		ExtendsStmt() { }
+
+		ExtendsStmt(NamedType* base, BlockExpr* body) : base(base), body(body) { }
 	};
 
 	//
 	// Expressions
 	//
 
-	struct Expression : Node { };
+	struct Expression : Node {
+	protected:
+		Expression() { }
+	};
 
 	struct TupleComponent : Expression {
 		std::string name;
 		Expression* value;
+
+		TupleComponent() { }
+
+		TupleComponent(std::string name, Expression* value) : name(name), value(value) { }
 	};
 
 	struct TupleExpr : Expression {
 		std::vector<TupleComponent*> components;
+
+		TupleExpr() { }
+
+		TupleExpr(std::vector<TupleComponent*> components) : components(components) { }
 	};
 
 	struct BinaryExpr : Expression {
 		Expression* LHS;
 		Expression* RHS;
 		Operator operation;
+
+		BinaryExpr() { }
+
+		BinaryExpr(Expression* LHS, Operator operation, Expression* RHS) :
+			LHS(LHS), operation(operation), RHS(RHS) { }
 	};
 
 	struct UnaryExpr : Expression {
 		Expression* value;
 		OpPosition position;
 		Operator operation;
+
+		UnaryExpr() { }
+
+		UnaryExpr(Expression* value, OpPosition position, Operator operation) :
+			value(value), position(position), operation(operation) { }
 	};
 
 	struct ArrayExpr : Expression {
 		std::vector<Expression*> values;
+
+		ArrayExpr() { }
+
+		ArrayExpr(std::vector<Expression*> values) : values(values) { }
 	};
 
 	struct ArrayAccessExpr : Expression {
 		Expression* LHS;
 		Expression* index;
+
+		ArrayAccessExpr() { }
+
+		ArrayAccessExpr(Expression* LHS, Expression* index) : LHS(LHS), index(index) { }
 	};
 
 	struct ArrayRangeExpr : Expression {
 		Expression* start;
 		Expression* end;
 		ArrayRangeType type;
+
+		ArrayRangeExpr() { }
+
+		ArrayRangeExpr(Expression* start, Expression* end, ArrayRangeType type) :
+			start(start), end(end), type(type) { }
 	};
 
 	struct BlockExpr : Expression {
 		std::vector<Node*> statements;
+
+		BlockExpr() { }
+
+		BlockExpr(std::vector<Node*> statements) : statements(statements) { }
 	};
 
 	struct ConditionalBlock : Expression {
 		Expression* condition;
 		BlockExpr* body;
+
+		ConditionalBlock() { }
+
+		ConditionalBlock(Expression* condition, BlockExpr* body) :
+			condition(condition), body(body) { }
 	};
 
 	struct IfExpr : Expression {
 		std::vector<ConditionalBlock*> blocks;
+
+		IfExpr() { }
+
+		IfExpr(std::vector<ConditionalBlock*> blocks) : blocks(blocks) { }
 	};
 
 	struct LoopExpr : Expression {
@@ -211,26 +330,49 @@ namespace orange { namespace ast {
 		Expression* condition;
 		Expression* afterthought;
 		LoopConditionCheck check;
+
+		LoopExpr(Expression* initializer, Expression* condition, Expression* afterthought) :
+			initializer(initializer), condition(condition), afterthought(afterthought) { }
 	};
 
 	struct StringExpr : Expression {
 		std::string value;
+
+		StringExpr() { }
+
+		StringExpr(std::string value) : value(value) { }
 	};
 
 	struct VarRefExpr : Expression {
 		std::string name;
+
+		VarRefExpr() { }
+
+		VarRefExpr(std::string name) : name(name) { }
 	};
 
 	struct IntExpr : Expression {
 		uint64_t value;
+
+		IntExpr() { }
+
+		IntExpr(uint64_t value) : value(value) { }
 	};
 
 	struct DoubleExpr : Expression {
 		double value;
+
+		DoubleExpr() { }
+
+		DoubleExpr(double value) : value(value) { }
 	};
 
 	struct FloatExpr : Expression {
 		float value;
+
+		FloatExpr() { }
+
+		FloatExpr(float value) : value(value) { }
 	};
 
 	struct FunctionExpr : Expression {
@@ -240,18 +382,30 @@ namespace orange { namespace ast {
 		std::vector<TypeConstraint*> constraints;
 		std::vector<ParamStmt*> params;
 		BlockExpr* body;
+
+		FunctionExpr() { }
+
+		FunctionExpr(Type* retType, std::string name, std::vector<Type*> generics,
+		             std::vector<TypeConstraint*> constraints, std::vector<ParamStmt*> params,
+		             BlockExpr* body) : retType(retType), name(name), generics(generics),
+		                                constraints(constraints), params(params), body(body) { }
 	};
 
 	struct FuncCallExpr : Expression {
 		Expression* function;
 		std::vector<Expression*> args;
+
+		FuncCallExpr() { }
+
+		FuncCallExpr(Expression* function, std::vector<Expression*> args) :
+			function(function), args(args) { }
 	};
 
 	/// Gets the next free ID for a node.
 	int getNextID();
 
 	template <class T, class... U>
-	T* CreateNode(U&&... params) {
+	T* CreateNode(U&& ... params) {
 		static_assert(std::is_base_of<Node, T>::value, "T must be a subclass of Node.");
 
 		T* node = new T(std::forward<U>(params)...);
