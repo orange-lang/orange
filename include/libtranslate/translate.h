d763c6bf9758fd87c8604808f487abf2214f7ff3
Robert Fratto <robertfratto@gmail.com>
2016-07-04 15:59:22 -0400

    Add libtranslate project
    Currently empty; link to LLVM

---
+++ include/libtranslate/translate.h
@@ -0,0 +1,9 @@
+//
+// Copyright 2014-2016 Robert Fratto. See the LICENSE.txt file at the top-level
+// directory of this distribution.
+//
+// Licensed under the MIT license <http://opensource.org/licenses/MIT>. This file
+// may not be copied, modified, or distributed except according to those terms.
+//
+
+#pragma once
