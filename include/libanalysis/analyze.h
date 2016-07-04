2aca46e62b1d597b07a1a9ef6ac3d4ec99ac041e
Robert Fratto <robertfratto@gmail.com>
2016-07-04 01:42:29 -0400

    Refine how types are stored
    A context has a a hierarchy, since there may be functions that are dependent on the context of a parent node.

--- include/libanalysis/analyze.h
+++ include/libanalysis/analyze.h
@@ -15,39 +15,65 @@
 #include <libast/type.h>
 
 namespace orange { namespace analysis {
-	/// Some structures support generic types and can be instantiated with different types. When
-	/// that happens, there are several instances of the same structure with different types that initialize
-	/// it (like parameters). These types are used as context to determine the type of all children nodes.
-	///
-	/// For generic blocks, the default context is whatever what was literally typed in a file.
+	/// The type of some node is dependent on a specific context. There always exists a global context,
+	/// but a context is created for each function and class. The default context for a function and class
+	/// is specified by the literal typed parameters and members for that node. Non-default contexts are created
+	/// for instances of functions/classes if the default context is generic.
 	class NodeTypeContext {
 	private:
-		/// Node ID this context is for
+		/// Node ID this context is for. -1 if global.
 		int mID;
 
-		/// The types of this context.
-		std::vector<orange::ast::Type*> mTypes;
+		/// The types that uniquely define this context
+		std::vector<orange::ast::Type*> mParameters;
+
+		/// Children contexts that rely on this node's context
+		/// The children defined here are for different nodes.
+		std::vector<NodeTypeContext*> mChildren;
+
+		/// The types defined for all the nodes in this context.
+		std::map<int, orange::ast::Type*> mTypes;
 	public:
 		/// Gets the node ID this context is for.
 		int NodeID() const;
+
+		/// Gets whether this context is global.
+		bool IsGlobalContext() const;
+
+		/// Gets all children contexts.
+		std::vector<NodeTypeContext*> GetChildrenContexts() const;
+
+		/// Gets all contexts tied a specific node. Searches contexts for any children where this
+		/// node also has a context.
+		std::vector<NodeTypeContext*> GetChildrenContexts(orange::ast::Node* node) const;
 
 		/// Gets the types for this context.
 		std::vector<orange::ast::Type*> GetTypes() const;
 
 		/// Gets whether this context is generic (i.e., any of the types to initialize it are of var type)
 		bool IsGeneric() const;
+
+		orange::ast::Type* GetNodeType(orange::ast::Node* node) const;
+
+		void SetNodeType(orange::ast::Node* node, orange::ast::Type* type) const;
 	};
 
+	/// The type table holds a reference to the global context and provides methods to get the type for any node.
+	/// It provides methods for searching for the type of a node, automating the work for searching for the
+	/// node throughout the type context tree.
 	class TypeTable {
 	private:
-		std::vector<orange::ast::LongBlockExpr*> mASTs;
-		std::vector<NodeTypeContext*> mContexts;
+		NodeTypeContext* mGlobalContext;
 	public:
-		/// Gets all contexts
-		std::vector<NodeTypeContext*> GetContexts() const;
+		/// Gets the global context.
+		NodeTypeContext* GetGlobalContext() const;
 
-		/// Gets all contexts tied a specific node.
+		/// Gets all contexts tied a specific node. Searches contexts for any children where this
+		/// node also has a context.
 		std::vector<NodeTypeContext*> GetContexts(orange::ast::Node* node) const;
+
+		/// Find the default context for a node.
+		NodeTypeContext* GetDefaultContext() const;
 
 		/// Gets the type of a node. A context may be passed in. If no context is specified, the default
 		/// context is used.
@@ -62,4 +88,7 @@ namespace orange { namespace analysis {
 
 		TypeResolution(std::vector<orange::ast::LongBlockExpr*> ASTs);
 	};
+
+	/// Validates that an AST is well-formed and isn't missing any required components
+	bool ValidateAST(orange::ast::LongBlockExpr* AST);
 }}
