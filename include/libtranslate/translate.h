94a1d58fa61a49f67dd6007c0a0e7cba3ce4ae55
Robert Fratto <robertfratto@gmail.com>
2016-07-08 18:17:27 -0400

    Map out basic interface for libtranslate

--- include/libtranslate/translate.h
+++ include/libtranslate/translate.h
@@ -7,3 +7,31 @@
 //
 
 #pragma once
+
+#include <libast/ast.h>
+#include <libanalysis/analyze.h>
+
+#include <llvm/IR/Module.h>
+#include <memory>
+#include <string>
+
+namespace orange { namespace translate {
+	class Translator {
+	private:
+		analysis::TypeTable* mTypeTable = nullptr;
+	public:
+		/// Gets the type table given to this AST.
+		analysis::TypeTable* GetTypeTable() const;
+
+		/// Translates an AST into a code module.
+		/// Code modules ignore most statements in the global block.
+		std::shared_ptr<llvm::Module> Translate(ast::LongBlockExpr* ast, std::string name);
+
+		// Translates an AST into a main module, complete with a main function.
+		std::shared_ptr<llvm::Module> TranslateMain(ast::LongBlockExpr* ast, std::string name);
+
+		/// Constructs a new translator. A translator must be given a type table 
+		/// that has type info for each AST that will be translated.
+		Translator(analysis::TypeTable* typeInfo);
+	};
+}}
--- lib/libtranslate/src/translate.cc
+++ lib/libtranslate/src/translate.cc
@@ -6,3 +6,27 @@
 // may not be copied, modified, or distributed except according to those terms.
 //
 
+#include <libtranslate/translate.h>
+
+#include <iostream>
+#include <llvm/IR/LLVMContext.h>
+
+using namespace orange::ast;
+using namespace orange::analysis; 
+using namespace orange::translate;
+
+TypeTable* Translator::GetTypeTable() const { 
+	return mTypeTable;
+}
+
+std::shared_ptr<llvm::Module> Translator::Translate(ast::LongBlockExpr* ast, std::string name) { 
+	auto module = std::make_shared<llvm::Module>(name, llvm::getGlobalContext());
+	return module;
+}
+
+std::shared_ptr<llvm::Module> Translator::TranslateMain(ast::LongBlockExpr* ast, std::string name) { 
+	auto module = std::make_shared<llvm::Module>(name, llvm::getGlobalContext());
+	return module;
+}
+
+Translator::Translator(analysis::TypeTable* typeInfo) : mTypeTable(typeInfo) { }
