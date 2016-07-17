313d4b2df459971378b344f3af59c3941112b02c
Robert Fratto <robertfratto@gmail.com>
2016-07-17 02:24:26 -0400

    Move TokenType -> string map to lex.h

--- lib/libparse/include/lex.h
+++ lib/libparse/include/lex.h
@@ -9,6 +9,7 @@
 #pragma once
 
 #include <string>
+#include <map>
 #include <istream>
 
 namespace orange { namespace parser {
@@ -32,6 +33,41 @@ namespace orange { namespace parser {
 		DEF, ARROW, EXTERN, AGGREGATE, INTERFACE, TILDE, NAMESPACE, IMPORT, NEW,
 		DELETE, PRIVATE, PROTECTED, PUBLIC, GET, SET, VIRTUAL, FINAL, WHERE, DATA,
 		EXTEND, NOT, CONST, TRY, CATCH, FINALLY, THROW, OF, AS, PROPERTY, THIS, BOOL
+	};
+
+	static std::map<TokenType, std::string> TokenDescriptionMap = {
+		{TOKEN_EOF, "EOF"}, {VAL_INT, "int value"}, {VAL_INT8, "int8 value"}, {VAL_INT16, "int16 value"},
+		{VAL_INT32, "int32 value"}, {VAL_INT64, "int64 value"}, {VAL_UINT, "uint value"}, {VAL_UINT8, "uint8 value"},
+		{VAL_UINT16, "uint16 value"}, {VAL_UINT32, "uint32 value"}, {VAL_UINT64, "uint64 value"}, {VAL_FLOAT, "float value"},
+		{VAL_DOUBLE, "double value"}, {VAL_STRING, "string value"}, {VAL_CHAR, "char value"}, {VAL_BOOL, "bool value"},
+		{COMMENT, "comment"}, {FLOAT, "float"}, {STRING, "string"}, {NEWLINE, "newline"},
+		{SEMICOLON, ";"}, {INT, "int"}, {UINT, "uint"}, {INT8, "int8"},
+		{INT16, "int16"}, {INT32, "int32"}, {INT64, "int64"}, {UINT8, "uint8"},
+		{UINT16, "uint16"}, {UINT32, "uint32"}, {UINT64, "uint64"}, {DOUBLE, "double"},
+		{CHAR, "char"}, {VAR, "var"}, {VOID, "void"}, {IDENTIFIER, "identifier"},
+		{TEMP, "_"}, {OPERATOR, "operator"}, {OPEN_PAREN, "("}, {CLOSE_PAREN, ")"},
+		{PLUS, "+"}, {MINUS, "-"}, {DIVIDE, "/"}, {TIMES, "*"},
+		{REMAINDER, "%"}, {BIT_OR, "|"}, {BIT_AND, "&"}, {BIT_XOR, "^"},
+		{QUESTION, "?"}, {SHIFT_LEFT, "<<"}, {SHIFT_RIGHT, ">>"}, {ASSIGN, "="},
+		{EQUALS, "=="}, {PLUS_ASSIGN, "+="}, {MINUS_ASSIGN, "-="}, {TIMES_ASSIGN, "*="},
+		{DIVIDE_ASSIGN, "/="}, {REMAINDER_ASSIGN, "%="}, {SHIFT_LEFT_ASSIGN, "<<="}, {SHIFT_RIGHT_ASSIGN, ">>="},
+		{BIT_OR_ASSIGN, "|="}, {BIT_AND_ASSIGN, "&="}, {BIT_XOR_ASSIGN, "^="}, {LESS_THAN, "<"},
+		{GREATER_THAN, ">"}, {LEQ, "<="}, {GEQ, ">="}, {NEQ, "!="},
+		{AND, "&&"}, {OR, "||"}, {INCREMENT, "++"}, {DECREMENT, "--"},
+		{ENUM, "enum"}, {OPEN_CURLY, "{"}, {CLOSE_CURLY, "}"}, {COMMA, ","},
+		{DOT, "."}, {CLASS, "class"}, {PARTIAL, "partial"}, {OPEN_BRACKET, "["},
+		{CLOSE_BRACKET, "]"}, {EXCLUSIVE_RANGE, "..."}, {INCLUSIVE_RANGE, ".."}, {COLON, ":"},
+		{IN, "in"}, {IF, "if"}, {ELIF, "elif"}, {ELSE, "else"},
+		{FOR, "for"}, {FOREACH, "foreach"}, {WHILE, "while"}, {FOREVER, "forever"},
+		{DO, "do"}, {SWITCH, "switch"}, {BREAK, "break"}, {CONTINUE, "continue"},
+		{YIELD, "yield"}, {DEF, "def"}, {ARROW, "->"}, {EXTERN, "extern"},
+		{AGGREGATE, "aggregate"}, {INTERFACE, "interface"}, {TILDE, "~"}, {NAMESPACE, "namespace"},
+		{IMPORT, "import"}, {NEW, "new"}, {DELETE, "delete"}, {PRIVATE, "private"},
+		{PROTECTED, "protected"}, {PUBLIC, "public"}, {GET, "get"}, {SET, "set"},
+		{VIRTUAL, "virtual"}, {FINAL, "final"}, {WHERE, "where"}, {DATA, "data"},
+		{EXTEND, "extend"}, {NOT, "!"}, {CONST, "const"}, {TRY, "try"},
+		{CATCH, "catch"}, {FINALLY, "finally"}, {THROW, "throw"}, {OF, "of"},
+		{AS, "as"}, {PROPERTY, "property"}, {THIS, "this"}, {BOOL, "bool"}
 	};
 
 	class Token {
--- lib/libparse/src/parser.cc
+++ lib/libparse/src/parser.cc
@@ -16,41 +16,6 @@
 using namespace orange::ast;
 using namespace orange::parser;
 
-static std::map<TokenType, std::string> TokenTyMap = {
-	{TokenType::TOKEN_EOF, "EOF"}, {TokenType::VAL_INT, "int value"}, {TokenType::VAL_INT8, "int8 value"}, {TokenType::VAL_INT16, "int16 value"},
-	{TokenType::VAL_INT32, "int32 value"}, {TokenType::VAL_INT64, "int64 value"}, {TokenType::VAL_UINT, "uint value"}, {TokenType::VAL_UINT8, "uint8 value"},
-	{TokenType::VAL_UINT16, "uint16 value"}, {TokenType::VAL_UINT32, "uint32 value"}, {TokenType::VAL_UINT64, "uint64 value"}, {TokenType::VAL_FLOAT, "float value"},
-	{TokenType::VAL_DOUBLE, "double value"}, {TokenType::VAL_STRING, "string value"}, {TokenType::VAL_CHAR, "char value"}, {TokenType::VAL_BOOL, "bool value"},
-	{TokenType::COMMENT, "comment"}, {TokenType::FLOAT, "float"}, {TokenType::STRING, "string"}, {TokenType::NEWLINE, "newline"},
-	{TokenType::SEMICOLON, ";"}, {TokenType::INT, "int"}, {TokenType::UINT, "uint"}, {TokenType::INT8, "int8"},
-	{TokenType::INT16, "int16"}, {TokenType::INT32, "int32"}, {TokenType::INT64, "int64"}, {TokenType::UINT8, "uint8"},
-	{TokenType::UINT16, "uint16"}, {TokenType::UINT32, "uint32"}, {TokenType::UINT64, "uint64"}, {TokenType::DOUBLE, "double"},
-	{TokenType::CHAR, "char"}, {TokenType::VAR, "var"}, {TokenType::VOID, "void"}, {TokenType::IDENTIFIER, "identifier"},
-	{TokenType::TEMP, "_"}, {TokenType::OPERATOR, "operator"}, {TokenType::OPEN_PAREN, "("}, {TokenType::CLOSE_PAREN, ")"},
-	{TokenType::PLUS, "+"}, {TokenType::MINUS, "-"}, {TokenType::DIVIDE, "/"}, {TokenType::TIMES, "*"},
-	{TokenType::REMAINDER, "%"}, {TokenType::BIT_OR, "|"}, {TokenType::BIT_AND, "&"}, {TokenType::BIT_XOR, "^"},
-	{TokenType::QUESTION, "?"}, {TokenType::SHIFT_LEFT, "<<"}, {TokenType::SHIFT_RIGHT, ">>"}, {TokenType::ASSIGN, "="},
-	{TokenType::EQUALS, "=="}, {TokenType::PLUS_ASSIGN, "+="}, {TokenType::MINUS_ASSIGN, "-="}, {TokenType::TIMES_ASSIGN, "*="},
-	{TokenType::DIVIDE_ASSIGN, "/="}, {TokenType::REMAINDER_ASSIGN, "%="}, {TokenType::SHIFT_LEFT_ASSIGN, "<<="}, {TokenType::SHIFT_RIGHT_ASSIGN, ">>="},
-	{TokenType::BIT_OR_ASSIGN, "|="}, {TokenType::BIT_AND_ASSIGN, "&="}, {TokenType::BIT_XOR_ASSIGN, "^="}, {TokenType::LESS_THAN, "<"},
-	{TokenType::GREATER_THAN, ">"}, {TokenType::LEQ, "<="}, {TokenType::GEQ, ">="}, {TokenType::NEQ, "!="},
-	{TokenType::AND, "&&"}, {TokenType::OR, "||"}, {TokenType::INCREMENT, "++"}, {TokenType::DECREMENT, "--"},
-	{TokenType::ENUM, "enum"}, {TokenType::OPEN_CURLY, "{"}, {TokenType::CLOSE_CURLY, "}"}, {TokenType::COMMA, ","},
-	{TokenType::DOT, "."}, {TokenType::CLASS, "class"}, {TokenType::PARTIAL, "partial"}, {TokenType::OPEN_BRACKET, "["},
-	{TokenType::CLOSE_BRACKET, "]"}, {TokenType::EXCLUSIVE_RANGE, "..."}, {TokenType::INCLUSIVE_RANGE, ".."}, {TokenType::COLON, ":"},
-	{TokenType::IN, "in"}, {TokenType::IF, "if"}, {TokenType::ELIF, "elif"}, {TokenType::ELSE, "else"},
-	{TokenType::FOR, "for"}, {TokenType::FOREACH, "foreach"}, {TokenType::WHILE, "while"}, {TokenType::FOREVER, "forever"},
-	{TokenType::DO, "do"}, {TokenType::SWITCH, "switch"}, {TokenType::BREAK, "break"}, {TokenType::CONTINUE, "continue"},
-	{TokenType::YIELD, "yield"}, {TokenType::DEF, "def"}, {TokenType::ARROW, "->"}, {TokenType::EXTERN, "extern"},
-	{TokenType::AGGREGATE, "aggregate"}, {TokenType::INTERFACE, "interface"}, {TokenType::TILDE, "~"}, {TokenType::NAMESPACE, "namespace"},
-	{TokenType::IMPORT, "import"}, {TokenType::NEW, "new"}, {TokenType::DELETE, "delete"}, {TokenType::PRIVATE, "private"},
-	{TokenType::PROTECTED, "protected"}, {TokenType::PUBLIC, "public"}, {TokenType::GET, "get"}, {TokenType::SET, "set"},
-	{TokenType::VIRTUAL, "virtual"}, {TokenType::FINAL, "final"}, {TokenType::WHERE, "where"}, {TokenType::DATA, "data"},
-	{TokenType::EXTEND, "extend"}, {TokenType::NOT, "!"}, {TokenType::CONST, "const"}, {TokenType::TRY, "try"},
-	{TokenType::CATCH, "catch"}, {TokenType::FINALLY, "finally"}, {TokenType::THROW, "throw"}, {TokenType::OF, "of"},
-	{TokenType::AS, "as"}, {TokenType::PROPERTY, "property"}, {TokenType::THIS, "this"}, {TokenType::BOOL, "bool"}
-};
-
 namespace orange { namespace parser { namespace impl {
 	using namespace orange::ast;
 
@@ -63,26 +28,26 @@ namespace orange { namespace parser { namespace impl {
 		void Expected(std::string expected) {
 			auto tok = GetNextConcreteToken();
 			auto str = tok->value;
-			if (str == "" || str == "\n") str = TokenTyMap[tok->type];
+			if (str == "" || str == "\n") str = TokenDescriptionMap[tok->type];
 
 			std::cerr << "Expected " << expected << ", got " << str << std::endl;
 		}
 
 		void Expected(std::string expected, Token* tok) {
 			auto str = tok->value;
-			if (str == "" || str == "\n") str = TokenTyMap[tok->type];
+			if (str == "" || str == "\n") str = TokenDescriptionMap[tok->type];
 
 			std::cerr << "Expected " << expected << ", got " << str << std::endl;
 		}
 
 		bool Expect(TokenType ty, Token* tok) {
-			if (tok->type != ty) Expected(TokenTyMap[ty], tok);
+			if (tok->type != ty) Expected(TokenDescriptionMap[ty], tok);
 			return tok->type == ty;
 		}
 
 		bool Expect(TokenType ty) {
 			auto next = GetNextConcreteToken();
-			if (next->type != ty) Expected(TokenTyMap[ty], next);
+			if (next->type != ty) Expected(TokenDescriptionMap[ty], next);
 			return next->type == ty;
 		}
 
