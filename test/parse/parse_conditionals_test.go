6d6b51a3b680515eeb72dba70fda3590331730a6
Robert Fratto <robertfratto@gmail.com>
2017-04-08 13:47:19 -0400

    Move conditional parsing tests to separate file

---
+++ test/parse/parse_conditionals_test.go
@@ -0,0 +1,87 @@
+package test
+
+import (
+	. "github.com/onsi/ginkgo/extensions/table"
+
+	"github.com/orange-lang/orange/ast"
+)
+
+var _ = DescribeTable("Parsing conditionals", expectNode,
+	Entry("if", `if (5) {
+			1
+			2
+		}`, &ast.IfStmt{
+		Condition: &ast.IntExpr{Value: 5, Size: 64},
+		Body: &ast.BlockStmt{Nodes: []ast.Node{
+			&ast.IntExpr{Value: 1, Size: 64},
+			&ast.IntExpr{Value: 2, Size: 64},
+		}},
+	}),
+
+	Entry("elif", `if (5) {
+			1
+			2
+		} elif (6) {
+			3
+			4
+	  } elif (7) {
+		  5
+			6	
+		}`, &ast.IfStmt{
+		Condition: &ast.IntExpr{Value: 5, Size: 64},
+		Body: &ast.BlockStmt{Nodes: []ast.Node{
+			&ast.IntExpr{Value: 1, Size: 64},
+			&ast.IntExpr{Value: 2, Size: 64},
+		}},
+		Else: &ast.IfStmt{
+			Condition: &ast.IntExpr{Value: 6, Size: 64},
+			Body: &ast.BlockStmt{Nodes: []ast.Node{
+				&ast.IntExpr{Value: 3, Size: 64},
+				&ast.IntExpr{Value: 4, Size: 64},
+			}},
+			Else: &ast.IfStmt{
+				Condition: &ast.IntExpr{Value: 7, Size: 64},
+				Body: &ast.BlockStmt{Nodes: []ast.Node{
+					&ast.IntExpr{Value: 5, Size: 64},
+					&ast.IntExpr{Value: 6, Size: 64},
+				}},
+			},
+		},
+	}),
+
+	Entry("elif and else", `if (5) {
+			1
+		} elif (6) {
+			2
+	  } else {
+		  3
+		}`, &ast.IfStmt{
+		Condition: &ast.IntExpr{Value: 5, Size: 64},
+		Body: &ast.BlockStmt{Nodes: []ast.Node{
+			&ast.IntExpr{Value: 1, Size: 64},
+		}},
+		Else: &ast.IfStmt{
+			Condition: &ast.IntExpr{Value: 6, Size: 64},
+			Body: &ast.BlockStmt{Nodes: []ast.Node{
+				&ast.IntExpr{Value: 2, Size: 64},
+			}},
+			Else: &ast.BlockStmt{
+				Nodes: []ast.Node{&ast.IntExpr{Value: 3, Size: 64}},
+			},
+		},
+	}),
+
+	Entry("else", `if (5) {
+			1
+		} else {
+		  2
+		}`, &ast.IfStmt{
+		Condition: &ast.IntExpr{Value: 5, Size: 64},
+		Body: &ast.BlockStmt{Nodes: []ast.Node{
+			&ast.IntExpr{Value: 1, Size: 64},
+		}},
+		Else: &ast.BlockStmt{
+			Nodes: []ast.Node{&ast.IntExpr{Value: 2, Size: 64}},
+		},
+	}),
+)
